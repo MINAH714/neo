fba440330f1553998910189f6527d9b0df801fe6
MINAH714 <wjdalsdk1344@naver.com>
2025-04-09 18:04:18 +0900

    up

--- python/pythonBasic/p43_blankgame.py
+++ python/pythonBasic/p43_blankgame.py
@@ -5,10 +5,10 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello Pygame World!!!!!!!')
+pygame.display.set_caption('Hello Pygame World!!')
 
 while True:
     for event in pygame.event.get():
-        if event.type == Quit:
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
