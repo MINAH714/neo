018be768f1b5bd168e6d4d16066b0f2a15703017
MINAH714 <jungmina1344@gmail.com>
2025-04-11 14:14:24 +0900

    up

--- python/pythonData/p203_makecsv.py
+++ python/pythonData/p203_makecsv.py
@@ -6,11 +6,11 @@ myColumns = ['번호', '이름', '나이']
 myencoding = 'utf-8'
 
 for idx in range(1, 3):
-  sublist = []
-  sublist.append(100 * idx)
-  sublist.append('김철수' + str(idx))
-  sublist.append(random.randint(1, 10))
-  result.append(sublist)
+    sublist = []
+    sublist.append(100 * idx)
+    sublist.append('김철수' + str(idx))
+    sublist.append((random.randint(1, 10)))
+    result.append(sublist)
 
 myframe = pd.DataFrame(result, columns=myColumns)
 
@@ -27,4 +27,4 @@ filename = 'csv_04.csv'
 myframe.to_csv(filename, encoding=myencoding, mode='w', index=False, sep='%')
 
 for _ in range(1, 5):
-  print(f'csv_0{_}.csv 파일 저장 완료~!!')
+    print(f'csv_0{_}.csv 파일 저장 완료~!!')
